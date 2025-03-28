df75255a38727f405edd48fa9ce2c807ea6516f3
fernsky <trilochanbhusal000@gmail.com>
2025-03-28 10:26:36 +0545

    feat: update app routes and facade changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,12 @@
 import { Routes } from '@angular/router';
 import { RoleType } from './core/models/role.enum';
 import { authGuard } from './core/guards/auth.guard';
+import { publicGuard } from './core/guards/public.guard';
 
 export const routes: Routes = [
   {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'dashboard',
-  },
-  {
     path: 'auth',
+    canActivate: [publicGuard],
     loadChildren: () =>
       import('./features/auth/auth.routes').then((m) => m.AUTH_ROUTES),
   },
@@ -29,6 +26,11 @@ export const routes: Routes = [
     },
     loadComponent: () =>
       import('./features/admin/admin.component').then((m) => m.AdminComponent),
+  },
+  {
+    path: '',
+    redirectTo: 'auth/login',
+    pathMatch: 'full',
   },
   {
     path: '**',
