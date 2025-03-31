de1ac44d72d12a451ff7d50d6a68db7f38fd1109
fernsky <trilochanbhusal000@gmail.com>
2025-03-31 06:06:27 +0545

    feat: implement base authentication component with Carbon Design System integration and responsive layout

--- src/app/features/auth/components/base-auth/base-auth.component.ts
+++ src/app/features/auth/components/base-auth/base-auth.component.ts
@@ -3,30 +3,14 @@ import { CommonModule } from '@angular/common';
 import { BackgroundParticlesComponent } from '@app/shared/components/background-particles/background-particles.component';
 import { GovBrandingComponent } from '@app/shared/components/gov-branding/gov-branding.component';
 import { SystemFeaturesComponent } from '@app/shared/components/system-features/system-features.component';
-import { MatCardModule } from '@angular/material/card';
 import { RouterOutlet } from '@angular/router';
+
+// Import Carbon components
+import { GridModule } from 'carbon-components-angular';
 
 @Component({
   selector: 'app-base-auth',
-  template: `
-    <div class="auth-container">
-      <div class="background-decorations">
-        <div class="curved-line"></div>
-      </div>
-      <app-background-particles></app-background-particles>
-
-      <div class="auth-header">
-        <app-gov-branding></app-gov-branding>
-      </div>
-
-      <div class="auth-content">
-        <app-system-features></app-system-features>
-        <mat-card>
-          <router-outlet></router-outlet>
-        </mat-card>
-      </div>
-    </div>
-  `,
+  templateUrl: './base-auth.component.html',
   styleUrls: ['./base-auth.component.scss'],
   standalone: true,
   imports: [
@@ -34,8 +18,8 @@ import { RouterOutlet } from '@angular/router';
     BackgroundParticlesComponent,
     GovBrandingComponent,
     SystemFeaturesComponent,
-    MatCardModule,
     RouterOutlet,
+    GridModule,
   ],
 })
 export class BaseAuthComponent {}
